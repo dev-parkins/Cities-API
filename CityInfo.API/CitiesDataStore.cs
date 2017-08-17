e03c8aa8e307937f696af1c2af8af137d2e7f61a
Christopher Parkins <christopher.parkins@infinity-ss.com>
2017-08-17 11:35:22 -0700

    Added support for point of interest child resource.

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -21,19 +21,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "Known as the Big Apple"
+                    Description = "Known as the Big Apple",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Urban Park to see!"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Skyscraper worlds high"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Phoenix",
-                    Description = "'Hot as Heck' - Everyone"
+                    Description = "'Hot as Heck' - Everyone",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Papago Park",
+                            Description = "Great Hiking spot near Chris' Home"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Arizona State University",
+                            Description = "Phoenix's prime university"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Lake Havasu City",
-                    Description = "'Hotter than Phoenix' - All residents"
+                    Description = "'Hotter than Phoenix' - All residents",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Havasu Lake",
+                            Description = "Hosted MTV show in the 90's"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "London Bridge",
+                            Description = "Original bridge brought from London stone-by-stone."
+                        }
+                    }
                 }
             };
         }
