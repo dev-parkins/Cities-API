083f0b188e9f41c0e154a785b4c54a632e6c5a36
Christopher Parkins <christopher.parkins@infinity-ss.com>
2017-08-17 11:03:05 -0700

    Initial Commit for Cities API Project

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        // = new CitiesDataStore() allows assigning on init, good for readonly
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            //dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "Known as the Big Apple"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Phoenix",
+                    Description = "'Hot as Heck' - Everyone"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Lake Havasu City",
+                    Description = "'Hotter than Phoenix' - All residents"
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+namespace CityInfo.API.Controllers
+{
+    //Route [Route("api/[controller]")] //Will Get name from controller class
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        //[HttpGet("api/cities/{id}")] //Since we defined Route at Class-level, can just use last part of URI
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(dto => dto.Id == id));
+        }
+    }
+}
